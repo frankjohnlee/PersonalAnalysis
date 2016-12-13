cee9712ccc0181606479212da401de282e4ea5a1
Matthew Scicluna <mattcscicluna@gmail.com>
2016-12-13 14:56:02 -0500

    added dict of permissions logons

--- Website.py
+++ Website.py
@@ -5,26 +5,29 @@ import flask_bootstrap
 FLASK_DEBUG=1
 CLIENT_ID = '2285WZ'
 CLIENT_SECRET = '8e0c01e96edd6f3dafe6ebf63cdf28b2'
-FITBIT_API = 'https://www.fitbit.com/oauth2/authorize?' \
-             'response_type=code&client_id={}&redirect_uri=http://localhost:8080/' \
-             '&scope=activity%20nutrition%20heartrate%20location%20nutrition' \
-             '%20profile%20settings%20sleep%20social%20weight'.format(CLIENT_ID)
+REDIRECT='http://localhost:80/'
+API_DICT = {'fitbit' : 'https://www.fitbit.com/oauth2/authorize? '
+                       'response_type=code&client_id={0}&redirect_uri={1}'
+                       '&scope=activity%20nutrition%20heartrate%20location%20nutrition'
+                       '%20profile%20settings%20sleep%20social%20weight'.format(CLIENT_ID, REDIRECT)
+            }
+
 
 @app.route('/')
 def main_page():
-    return render_template('index.html', site=FITBIT_API)
+    return render_template('index.html')
 
 @app.route('/index.html')
 def main_page2():
-    return render_template('index.html', site=FITBIT_API)
+    return render_template('index.html')
 
 @app.route('/AccManager.html')
 def acc_manage():
-    return render_template('AccManager.html', site=FITBIT_API)
+    return render_template('AccManager.html')
 
 @app.route('/tables.html')
 def tables():
-    return render_template('tables.html', site=FITBIT_API)
+    return render_template('tables.html')
 
 if __name__ == '__main__':
       app.run(host="localhost", port=80)
