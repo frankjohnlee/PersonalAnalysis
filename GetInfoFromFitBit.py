594a6c6cd8996d8da5a42e6eb6b083df28464e78
Matthew Scicluna <mattcscicluna@gmail.com>
2016-12-14 12:27:37 -0500

    added template manager

--- GetInfoFromFitBit.py
+++ GetInfoFromFitBit.py
@@ -1,8 +1,43 @@
 import fitbit
+import urllib
+import base64
 
 CLIENT_ID = '2285WZ'
 CLIENT_SECRET = '8e0c01e96edd6f3dafe6ebf63cdf28b2'
+REDIRECT='http://localhost/AccManager.html'
+GRANT_TYPE = 'authorization_code'
 
-'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=2285WZ&redirect_uri=http://localhost:8080/&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight'
+#  This is the Fitbit URL
+TOKEN_URL = "https://api.fitbit.com/oauth2/token"
 
-unauth_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET)
+def authenticate_fitbit(code):
+    print('got here')
+
+    # Form the data payload
+    BodyText = {'code': code,
+                'redirect_uri': REDIRECT,
+                'client_id': CLIENT_ID,
+                'grant_type': GRANT_TYPE}
+
+    BodyURLEncoded = urllib.parse.urlencode(BodyText)
+
+    #  Start the request
+    req = urllib.request.Request(TOKEN_URL, BodyURLEncoded)
+
+    #  Add the headers, first we base64 encode the client id and client secret with a : in between
+    # and create the authorisation header
+    req.add_header('Authorization', 'Basic ' + base64.b64encode(CLIENT_ID + ":" + CLIENT_SECRET))
+    req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+
+    # Fire off the request
+    try:
+        response = urllib.urlopen(req)
+        FullResponse = response.read()
+        print(FullResponse)
+
+    except urllib.URLError as e:
+        print(e.code)
+        print(e.read())
+
+
+#unauth_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET)
--- Website.py
+++ Website.py
@@ -1,15 +1,14 @@
 from AddToDB import app
-from flask import render_template
-import flask_bootstrap
+from flask import render_template, request, url_for
+from GetInfoFromFitBit import authenticate_fitbit
 
-FLASK_DEBUG=1
+
+SERVER_HOST = "127.0.0.1"#SERVER_HOST = ""localhost""
+SERVER_PORT = 80
 CLIENT_ID = '2285WZ'
 CLIENT_SECRET = '8e0c01e96edd6f3dafe6ebf63cdf28b2'
-REDIRECT='http://localhost:80/'
-API_DICT = {'fitbit' : 'https://www.fitbit.com/oauth2/authorize? '
-                       'response_type=code&client_id={0}&redirect_uri={1}'
-                       '&scope=activity%20nutrition%20heartrate%20location%20nutrition'
-                       '%20profile%20settings%20sleep%20social%20weight'.format(CLIENT_ID, REDIRECT)
+REDIRECT='http://localhost/AccManager.html'
+API_DICT = {'fitbit':'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id={0}&redirect_uri={1}&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight'.format(CLIENT_ID, REDIRECT)
             }
 
 
@@ -17,17 +16,22 @@ API_DICT = {'fitbit' : 'https://www.fitbit.com/oauth2/authorize? '
 def main_page():
     return render_template('index.html')
 
+
 @app.route('/index.html')
 def main_page2():
     return render_template('index.html')
 
-@app.route('/AccManager.html')
+
+@app.route('/AccManager.html', methods=['GET'])
 def acc_manage():
-    return render_template('AccManager.html')
+    ##authenticate_fitbit(request.args)
+    return render_template('AccManager.html', sites=API_DICT)
+
 
 @app.route('/tables.html')
 def tables():
     return render_template('tables.html')
 
+
 if __name__ == '__main__':
-      app.run(host="localhost", port=80)
+      app.run(host=SERVER_HOST, port=SERVER_PORT)
