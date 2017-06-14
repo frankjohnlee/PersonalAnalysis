018c0f77afe565f9e10f37045f754b1474c5e254
FrankJohnLee <frankjohnlee@live.com>
2017-06-14 15:16:02 -0400

    adding changes and askin for support

--- Frontend/PersonalAnalysis/app/src/main/java/com/example/android/personalanalysis/MainActivity.java
+++ Frontend/PersonalAnalysis/app/src/main/java/com/example/android/personalanalysis/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.android.personalanalysis;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends"));
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
@@ -36,6 +46,37 @@ public class MainActivity extends AppCompatActivity {
                                 "Auth Token: "
                                 + loginResult.getAccessToken().getToken()
                 );
+
+                // To get extra information
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("LoginActivity", response.toString());
+
+                                // Application code
+                                String email = "";
+                                try {
+                                    email = object.getString("email");
+                                } catch (JSONException e) {
+                                    Log.e("MYAPP", "unexpected JSON exception for email", e);
+                                }
+//                                Log.d("email", email);
+
+                                String birthday = "";
+                                try {
+                                    birthday = object.getString("email");
+                                } catch (JSONException e) {
+                                    Log.e("MYAPP", "unexpected JSON exception birthday", e);
+                                }
+//                                Log.d("birthday", birthday);
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
 
             }
 
